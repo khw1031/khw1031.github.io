07c03361d97dd0df91f66d196760d1af62583775
dan <khw1031@gmail.com>
2019-07-18 14:54:20 +0900

    test doc

--- src/styles/mediaQuery.js
+++ src/styles/mediaQuery.js
@@ -1,8 +1,12 @@
 import { css } from 'styled-components'
 import { BreakPoints } from '../../custom/styleScheme'
 
-export const mediaQuery = screen => ([cssStr]) => css`
-  @media (min-width: ${BreakPoints[screen]}) {
+/**
+ * @param {string} breakpoint - breakpoint defined in styleScheme
+ * @return {function} return tag function that takes css String in styled component
+ */
+export const mediaQuery = breakpoint => ([cssStr]) => css`
+  @media (min-width: ${BreakPoints[breakpoint]}) {
     ${cssStr}
   }
 `
