cb02b85fbd6edcc839fb8705c461be7604e7402e
dan <khw1031@gmail.com>
2019-07-23 03:45:00 +0900

    init seo component

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,88 +1,115 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import { useStaticQuery, graphql } from 'gatsby'
+import urljoin from 'url-join/lib/url-join'
+import siteMeta from '../../custom/siteMeta'
 
-function SEO({ description, lang, meta, title }) {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            author
-          }
-        }
+export const SEO = ({ postNode, postPath, postSEO }) => {
+  let title
+  let description
+  let postUrl
+  let imageUrl = ''
+
+  if (postSEO) {
+    const postMeta = postNode.frontmatter
+    title = postMeta.title
+    description = postMeta.description ? postMeta.description : postNode.excerpt
+    if (postMeta.thumbnail) {
+      imageUrl = postMeta.thumbnail.childImageSharp.fixed.src
+    }
+    postUrl = urljoin(siteMeta.siteUrl, postPath)
+  } else {
+    title = siteMeta.siteTitle
+    description = siteMeta.siteDescription
+    imageUrl = siteMeta.siteLogo
+  }
+
+  imageUrl = urljoin(siteMeta.siteUrl, imageUrl)
+  const blogUrl = urljoin(siteMeta.siteUrl, siteMeta.pathPrefix)
+  const schemaOrgJSONLD = [
+    {
+      '@context': 'http://schema.org',
+      '@type': 'WebSite',
+      url: blogUrl,
+      name: title,
+      alternateName: siteMeta.siteTitleAlt ? siteMeta.siteTitleAlt : '',
+    },
+  ]
+
+  if (postSEO) {
+    schemaOrgJSONLD.push(
+      {
+        '@context': 'http://schema.org',
+        '@type': 'BreadcrumbList',
+        itemListElement: [
+          {
+            '@type': 'ListItem',
+            position: 1,
+            item: {
+              '@id': postUrl,
+              name: title,
+              imageUrl,
+            },
+          },
+        ],
+      },
+      {
+        '@context': 'http://schema.org',
+        '@type': 'BlogPosting',
+        url: blogUrl,
+        name: title,
+        alternateName: siteMeta.siteTitleAlt ? siteMeta.siteTitleAlt : '',
+        headline: title,
+        image: {
+          '@type': 'ImageObject',
+          url: imageUrl,
+        },
+        description,
       }
-    `
-  )
-
-  const metaDescription = description || site.siteMetadata.description
+    )
+  }
 
   return (
-    <Helmet
-      htmlAttributes={{
-        lang,
-      }}
-      title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-      ].concat(meta)}
-    />
+    <Helmet htmlAttributes={{ lang: 'ko' }}>
+      <meta name='description' content={description} />
+      <meta name='image' content={imageUrl} />
+      <script type='application/ld+json'>
+        {JSON.stringify(schemaOrgJSONLD)}
+      </script>
+
+      <meta property='og:url' content={postSEO ? postUrl : blogUrl} />
+      {postSEO ? <meta property='og:type' content='article' /> : null}
+      <meta property='og:title' content={title} />
+      <meta property='og:description' content={description} />
+      <meta property='og:image' content={imageUrl} />
+
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='twitter:title' content={title} />
+      <meta name='twitter:description' content={description} />
+      <meta name='twitter:image' content={imageUrl} />
+    </Helmet>
   )
 }
 
 SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
+  postSEO: false,
 }
 
 SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
+  postNode: PropTypes.shape({
+    excerpt: PropTypes.string,
+    frontmatter: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string,
+      thumbnail: PropTypes.shape({
+        childImageSharp: PropTypes.shape({
+          fixed: PropTypes.shape({
+            src: PropTypes.string,
+          }),
+        }),
+      }),
+    }).isRequired,
+  }).isRequired,
+  postPath: PropTypes.string.isRequired,
+  postSEO: PropTypes.bool,
 }
-
-export default SEO
