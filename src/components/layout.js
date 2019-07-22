d40d306a79b4414544a84be43d42a3bed528dd1d
dan <khw1031@gmail.com>
2019-07-23 03:44:46 +0900

    init layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,27 +1,41 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import { ThemeProvider } from 'styled-components'
+import styled, { ThemeProvider } from 'styled-components'
 // import { useStaticQuery, graphql } from 'gatsby'
-import Header from './header'
-import { useSiteTheme, GlobalStyle } from './styled/theme'
+import { useSiteTheme, GlobalStyle } from '../styles/theme'
+import { Header } from './header'
+import { BaseContainer } from '../styles/container'
+import { tabletAbove } from '../styles/mediaQuery'
 
 export const Layout = ({ children }) => {
   const [siteTheme, setSiteTheme] = useSiteTheme()
-
   return (
     <ThemeProvider theme={{ siteTheme }}>
       <>
         <GlobalStyle />
         <Helmet>
-          <meta name="description" content="test" />
+          <meta name='description' content='test' />
         </Helmet>
-        <Header siteTitle="test blog" setSiteTheme={setSiteTheme} />
-        <main>{children}</main>
+        <Header theme={siteTheme} setSiteTheme={setSiteTheme} />
+        <Main>
+          <Div maxWidth='800px'>{children}</Div>
+        </Main>
       </>
     </ThemeProvider>
   )
 }
+const Main = styled.main`
+  margin-top: 55px;
+  padding: 40px 0 0;
+  min-height: calc(100vh - 162px);
+  ${tabletAbove`
+    margin-top: 92px;
+    padding: 60px 0 0;
+  `}
+`
+
+const Div = styled(BaseContainer)``
 
 Layout.propTypes = {
   children: PropTypes.element.isRequired,
