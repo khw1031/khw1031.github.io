db88a5782ddec45d846635a64d92a30523f64fc4
dan <khw1031@gmail.com>
2019-07-23 03:40:28 +0900

    media quries

--- src/styles/mediaQuery.js
+++ src/styles/mediaQuery.js
@@ -1,12 +1,31 @@
 import { css } from 'styled-components'
-import { BreakPoints } from '../../custom/styleScheme'
+import { BreakPoints } from '../../custom/styleScheme/breakpoints'
 
-/**
- * @param {string} breakpoint - breakpoint defined in styleScheme
- * @return {function} return tag function that takes css String in styled component
- */
-export const mediaQuery = breakpoint => ([cssStr]) => css`
+const reduceCssStr = (cssStrs, keys) => {
+  return cssStrs
+    .reduce((prevTags, cssAttr, i) => {
+      return keys[i] ? [...prevTags, cssAttr, keys[i]] : [...prevTags, cssAttr]
+    }, [])
+    .join('')
+}
+
+const mediaQuery = breakpoint => (cssStrs, keys) => css`
   @media (min-width: ${BreakPoints[breakpoint]}) {
-    ${cssStr}
+    ${reduceCssStr(cssStrs, keys)}
   }
 `
+
+export const mobileSAbove = (cssStrs, ...keys) =>
+  mediaQuery('mobileS')(cssStrs, keys)
+export const mobileMAbove = (cssStrs, ...keys) =>
+  mediaQuery('mobileM')(cssStrs, keys)
+export const mobileLAbove = (cssStrs, ...keys) =>
+  mediaQuery('mobileL')(cssStrs, keys)
+export const tabletAbove = (cssStrs, ...keys) =>
+  mediaQuery('tablet')(cssStrs, keys)
+export const laptopAbove = (cssStrs, ...keys) =>
+  mediaQuery('laptop')(cssStrs, keys)
+export const laptopLAbove = (cssStrs, ...keys) =>
+  mediaQuery('laptopL')(cssStrs, keys)
+export const fourKAbove = (cssStrs, ...keys) =>
+  mediaQuery('fourK')(cssStrs, keys)
