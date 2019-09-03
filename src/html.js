69543d5af0aa9a220112ea2b8831e6093ea25062
khw1031 <khw1031@gmail.com>
2019-09-04 03:04:46 +0900

    Add viglink tag

--- src/html.js
+++ src/html.js
@@ -30,6 +30,21 @@ export default function HTML({
           dangerouslySetInnerHTML={{ __html: body }}
         />
         {postBodyComponents}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var vglnk = {key: 'ee216a5565c6278385bf52d9b5f1ceef'};
+              (function(d, t) {
+                var s = d.createElement(t);
+                  s.type = 'text/javascript';
+                  s.async = true;
+                  s.src = '//cdn.viglink.com/api/vglnk.js';
+                var r = d.getElementsByTagName(t)[0];
+                    r.parentNode.insertBefore(s, r);
+              }(document, 'script'));
+            `,
+          }}
+        />
       </body>
     </html>
   )
