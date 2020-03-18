e5f328329b65745ce387ca85545f4bcfeffa76b9
dan <khw1031@gmail.com>
2020-03-18 17:51:45 +0900

    Change font-family

--- custom/styleScheme/fonts.js
+++ custom/styleScheme/fonts.js
@@ -1,8 +1,9 @@
 export const fontScheme = {
-  fontStyle: 'normal',
-  fontVariant: 'normal',
-  baseFontSize: '16px',
-  fontWeight: 'normal',
-  lineHeight: '1.6',
-  fontFamily: '"Noto Sans KR", --apple-system, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif'
-}
+  fontStyle: "normal",
+  fontVariant: "normal",
+  baseFontSize: "16px",
+  fontWeight: "normal",
+  lineHeight: "1.6",
+  fontFamily:
+    '-apple-system, system-ui, BlinkMacSystemFont,"Segoe UI","Noto Sans KR", Roboto,"Helvetica Neue",Arial,sans-serif, ',
+};
--- gatsby-config.js
+++ gatsby-config.js
@@ -62,6 +62,7 @@ module.exports = {
         ],
       },
     },
+    `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-styled-components`,
       options: {
