f19b5a461c9f3f8862bbcc2f595ff445272147b6
khw1031 <khw1031@gmail.com>
2024-06-23 01:57:32 +0900

    update

--- samples/fp.ts
+++ samples/fp.ts
@@ -1,74 +1,33 @@
-// # 홀수 n개를 뽑아서 n^2 값을 모두 더하기
+import {
+  pipe,
+  range,
+  each,
+  map,
+  toArray,
+  reject,
+  reduce,
+  take,
+  split,
+  tap,
+  fromEntries,
+  noop,
+  filter,
+} from "ffuunnccttiioonn";
 
-function f1(limit: number, list: Iterable<number>) {
-  return pipe(
-    list,
-    filter((n) => n % 2),
-    map((n) => n * n),
-    take(3),
-    reduce((acc, n) => acc + n, 0),
-  );
-}
+const as = <T>(a: any) => a as T;
 
-const result = f1(2, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-console.log(result);
+const users = [
+  { id: 1, name: "John", age: 20 },
+  { id: 2, name: "Jane", age: 25 },
+  { id: 3, name: "Jim", age: 30 },
+];
 
-function* filter<A, B = unknown>(f: (a: A) => B, iterable?: Iterable<A>) {
-  if (iterable === undefined) {
-    return (iterable: Iterable<A>) => filter(f, iterable);
-  }
-  for (const a of iterable) {
-    if (f(a)) yield a;
-  }
-}
+const user = users.find((user) => user.name === "Kim");
+console.log(user);
 
-function* map<A, B = unknown>(f: (a: A) => B, iterable?: Iterable<A>) {
-  if (iterable === undefined) {
-    return (iterable: Iterable<A>) => map(f, iterable);
-  }
-  for (const a of iterable) {
-    yield f(a);
-  }
-}
-
-function* take<A>(length: number, iterable?: Iterable<A>) {
-  if (iterable === undefined) {
-    return (iterable: Iterable<A>) => take(length, iterable);
-  }
-  const iterator = iterable[Symbol.iterator]();
-  let cur = null;
-  while (length-- > 0 && (cur = iterator.next())) {
-    yield cur.value;
-  }
-}
-
-type SyncReducer<Acc, T> = (acc: Acc, value: T) => Acc;
-
-function reduce<T, Acc>(
-  f: SyncReducer<Acc, T>,
-  acc: Acc | Iterable<T>,
-  iterable?: Iterable<T>
-): Acc {
-  if (iterable === undefined) {
-    return (iterable: Iterable<T>) => reduce(f, acc, iterable);
-  }
-  for (const a of iterable) {
-    acc = f(acc as Acc, a);
-  }
-  return acc as Acc;
-}
-
-function pipe<T>(a: T, ...fns: any[]) {
-  return reduce(
-    (acc, f) => {
-      if (f[Symbol.iterator] === undefined) {
-        return f(acc as any);
-      }
-      const iter = f[Symbol.iterator]();
-      const fn = iter.next().value;
-      return fn(acc);
-    },
-    a,
-    fns
-  );
-}
+pipe(
+  users,
+  filter((user) => user.name === "Kim"),
+  take(1),
+  each(console.log)
+);
