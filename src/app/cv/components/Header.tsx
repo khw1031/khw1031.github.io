11e118da8ede690e183c61dfa67cf0ae399f2aa2
khw1031 <khw1031@gmail.com>
2025-12-10 23:25:20 +0900

    feat: add cv and post

--- src/app/cv/components/Header.tsx
+++ src/app/cv/components/Header.tsx
@@ -6,7 +6,9 @@ export function Header() {
       <div className="flex font-noto_serif items-end justify-between pb-8">
         <h1 className="text-neutral-800 text-md font-medium flex flex-col">
           <span>김 현 우</span>
-          <span className="text-sm text-gray-500">Frontend Developer</span>
+          <span className="text-sm text-gray-500">
+            Frontend Based Product Developer
+          </span>
         </h1>
         <div className="flex flex-col items-end">
           <a href="mailto:khw1031@gmail.com" className="text-sm">
